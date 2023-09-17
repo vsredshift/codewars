92f137cd49f9d9757813d0dea436e43d963fd80a
Martin O`Shaughnessy <vsredshift@gmail.com>
2023-09-17 20:26:16 +0200

    add first kyu7 example

---
+++ src/com/vsredshift/main/kyu7/RoundToNextMultipleOf5.java
@@ -0,0 +1,15 @@
+package com.vsredshift.main.kyu7;
+
+public class RoundToNextMultipleOf5 {
+
+    public static void main(String[] args) {
+        System.out.println(roundToNext5(10323));
+    }
+    
+    public static int roundToNext5(int number) {
+        while (number % 5 != 0) {
+            number++;
+        }
+        return number;
+    }
+}
